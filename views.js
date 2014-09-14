580c890f757860b90f1e9dc79c9c70fceec79897
tiny34 <tinynight_34@163.com>
2014-09-14 21:14:23 +0800

    支持手势滑动的html轮播插件

--- views.js
+++ views.js
@@ -5,42 +5,70 @@ views.Slider = function(options){
 	
 	this.ele = $();
 	
+	//private
+	//当前轮播点 实时更改
 	this.active = null;
+	//向左的便宜距离 实时更改
 	this.activeLeft = null;
+	//最小轮播
 	this.min = 0;
+	//最大轮播数  初始化时读取
 	this.max = 0;
+	//自动轮询时的计时器
+	this.timer = null;
+	//延迟重置的计时器
+	this.delayTimer = null; 
 	
+	//public
+	//轮播切换执行的时间
+	this.sliderTime = 200;
+	//自动轮播时的切换时间参数
 	this.loopTime = 3000;
+	//是否自动循环播放
 	this.auto  = true;
+	//是否从最后一个跳跃到第一个 或者 从第一个跳跃到最后一个 影响上翻和下翻
 	this.autoBack = true;
+	//轮播标记的触发类型 click 或者 mouseover 或者 mouseenter 都可以
 	this.pointType = "click";
 	
+	//轮播项最外层容器的标签名
 	this.class_content=".slider-content";
+	//上翻的标签名
 	this.class_prev=".slider-prev";
+	//下翻的标签名
 	this.class_next=".slider-next";
+	//滑块单位
 	this.class_panel=".slider-panel";
+	//滑块单位的横向容器 假想是无限大
 	this.class_panels=".slider-panels";
+	//滑块对应的导航
 	this.class_point=".slider-point";
+	//对应导航容器
 	this.class_points = ".slider-points";
 	
+	//序号
+	this.attr_index = "i";
+	//标记活跃时的类名
 	this.attr_active="on-active";
+	//读取导航事件类型的attribute
 	this.attr_pointType="pointType";
+	//轮播时间
 	this.attr_loopTime="loopTime";
+	//如果有值  不自动轮播
 	this.attr_auto="noAuto";
+	//如果有值 不自动从最后到第一个
 	this.attr_autoBack="noBack";
 	
-	this.events = {
-		go:new baseClass.Event()
-	};
-	
-	this.timer = null;
-	
-	baseMethod.extend(this,options);
-	this.init();
+	this.init(options);
 	this.bindEvents();
 };
 views.Slider.prototype = {
-	init:function(){
+	init:function(options){
+		
+		for(var x in options){
+			this[x] = options[x];
+		}
+		
 		var $ele = this.ele;
 		
 		var loopTime = parseInt($ele.attr(this.attr_loopTime));
@@ -62,20 +90,16 @@ views.Slider.prototype = {
 		}
 		
 		var $panel = $ele.find(this.class_panel);
+		var index = this.attr_index;
+		$panel.each(function(i,item){
+			$(this).attr(index,i);
+		});
 		var $con = $ele.find(this.class_panels);
-		this.min = 0;
-		this.max = $panel.length - 1;
-		
-		
-		var cloneFirst = $panel.eq(this.min).clone();
-		var cloneLast = $panel.eq(this.max).clone();
-		$panel.last().after(cloneLast);
-		$panel.last().after(cloneFirst);
-		cloneLast.css({"left":"-100%","position":"relative"});
-		
-		$panel = $ele.find(this.class_panel);
 		$con.css({"width":($panel.length*100)+"%"});
 		$panel.css({"width":(100/$panel.length)+"%"});
+		
+		this.min = 0;
+		this.max = $panel.length - 1;
 		
 		this.go(this.min);
 	},
@@ -102,7 +126,6 @@ views.Slider.prototype = {
 				startX = touch.pageX;
 				moveX = 0;
 				
-				$con.addClass("on-touch");
 				_this.stop();
 				return false;
 			},
@@ -121,26 +144,6 @@ views.Slider.prototype = {
 			},
 			"touchend":function(){
 				
-				var min = _this.min;
-				var max = _this.max;
-				var active = _this.active;
-				var width = $panel.eq(max).position().left - $panel.eq(min).position().left;			
-				var left = $con.position().left;
-				
-				if((moveX - startX)>30 && active == min){
-					left = left + width;
-				}else if((startX - moveX)>30 && active == max){
-					left = left - width;
-				}
-				$con.css({
-					"-webkit-transform": "translate3d("+left+"px, 0px, 0px)",
-					"-moz-transform": "translate3d("+left+"px, 0px, 0px)",
-					"transform": "translate3d("+left+"px, 0px, 0px)"
-				});
-				
-				$con.removeClass("on-touch");
-				_this.start();
-					
 				if((moveX - startX)>30){
 					_this.prev();
 				}else if((startX - moveX)>30){
@@ -149,6 +152,7 @@ views.Slider.prototype = {
 					_this.go();
 				}
 				
+				_this.start();
 				return false;
 			}
 		});
@@ -207,43 +211,66 @@ views.Slider.prototype = {
 		}
 	},
 	go:function(n){
+		
 		if(n<this.min || n>this.max){
 			return;
 		}
 		if(typeof n == "undefined"){
 			n = this.active;
 		}
+		window.clearTimeout(this.delayTimer);
 		
-		var $ele = this.ele;
 		var $panel = this.ele.find(this.class_panel);
 		var $point = this.ele.find(this.class_point);
 		var $con = this.ele.find(this.class_panels);
+		var $nextActive = $panel.filter("["+this.attr_index+"='"+n+"']");
 		
 		$panel.removeClass(this.attr_active);
-		$point.removeClass(this.attr_active);
-		$panel.eq(n).addClass(this.attr_active);
-		$point.eq(n).addClass(this.attr_active);
+		$point.removeClass(this.attr_active).eq(n).addClass(this.attr_active);
+		$nextActive.addClass(this.attr_active);
 		
-		var list = $ele.attr("class").split(" ");
-		var x=0;
-		for(;x<list.length;x++){
-			var c = list[x];
-			if(c.indexOf(this.attr_active)==0){
-				$ele.removeClass(c);
-			}
-		}
-		$ele.addClass(this.attr_active+"-"+n);
-		
-		var left = $panel.eq(n).position().left - $panel.first().position().left;
+		var left = $nextActive.position().left - $panel.first().position().left;
 		$con.css({
-			"-webkit-transform": "translate3d(-"+left+"px, 0px, 0px)",
-			"-moz-transform": "translate3d(-"+left+"px, 0px, 0px)",
+			"transition-duration":this.sliderTime+"ms",
+			//"-webkit-transform": "translate3d(-"+left+"px, 0px, 0px)",
+			//"-moz-transform": "translate3d(-"+left+"px, 0px, 0px)",
 			"transform": "translate3d(-"+left+"px, 0px, 0px)"
 		});
 	
-		this.events.go.notify(n);
 		this.active = n;
 		this.activeLeft = left;
+		
+		var _this = this;
+		this.delayTimer = window.setTimeout(function(){
+				_this.delayInit();
+		}, this.sliderTime+10);
+	},
+	delayInit:function(){
+		
+		var $con = this.ele.find(this.class_panels);
+		var $panel = this.ele.find(this.class_panel);
+		var $active = $panel.filter("."+this.attr_active);
+		
+		if($active.prevAll().length < 1){
+			$panel.first().before($panel.last());
+		}else if($active.prevAll().length > 1){
+			$panel.last().after($panel.first());
+		}
+		
+		var $panel = this.ele.find(this.class_panel);
+		var $active = $panel.filter("."+this.attr_active);
+		var left = $active.position().left - $panel.first().position().left;
+		this.activeLeft = left;
+		
+		$con.css({
+			//"-moz-transition-time":0,	/* Firefox 4 */
+			//"-webkit-transition-time":0,	/* Safari 和 Chrome */
+			//"-o-transition-time":0,	/* Opera */
+			"transition-duration":"0ms",
+			//"-webkit-transform": "translate3d(-"+left+"px, 0px, 0px)",
+			//"-moz-transform": "translate3d(-"+left+"px, 0px, 0px)",
+			"transform": "translate3d(-"+left+"px, 0px, 0px)"
+		});
 	}
 };
 
